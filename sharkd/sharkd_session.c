5eca855cfe7fdd5a36eb4dc5108d62a842ac2653
Jakub Zawadzki <darkjames-ws@darkjames.pl>
2017-03-19 21:12:18 +0100

    support exporting ssl sessions - easy prey.

--- sharkd/sharkd_session.c
+++ sharkd/sharkd_session.c
@@ -43,6 +43,8 @@
 
 #include <epan/column.h>
 
+#include <ui/ssl_key_export.h>
+
 #include <epan/stats_tree_priv.h>
 #include <epan/stat_tap_ui.h>
 #include <epan/conversation_table.h>
@@ -3118,6 +3120,25 @@ sharkd_session_process_download(char *buf, const jsmntok_t *tokens, int count)
 			json_print_base64(eo_entry->payload_data, (int) eo_entry->payload_len); /* XXX, export object will be truncated if >= 2^31 */
 			printf("}\n");
 		}
+	}
+	else if (!strcmp(tok_token, "ssl-sessions"))
+	{
+		char *str = ssl_export_sessions();
+
+		if (str)
+		{
+			const char *mime     = "text/plain";
+			const char *filename = tok_token;
+
+			printf("{\"file\":");
+			json_puts_string(filename);
+			printf(",\"mime\":");
+			json_puts_string(mime);
+			printf(",\"data\":");
+			json_print_base64(str, strlen(str));
+			printf("}\n");
+		}
+		g_free(str);
 	}
 }
 
