LiMinggang/webshark

commit 725a7ec2b113543de74ed15e70960ee6fd7b6370

webshark: create urls with encodeURIComponent() parameters.

web/js/webshark-tap.js
@@ -373,7 +373,11 @@ function webshark_create_tap_action_common(data)
 		var anal_a = document.createElement('a');
 
 		anal_a.setAttribute("target", "_blank");
-		anal_a.setAttribute("href", window.webshark.webshark_get_url() + "&tap=" + encodeURIComponent(data['_analyse']));
+		anal_a.setAttribute("href", window.webshark.webshark_create_url(
+			{
+				file: g_webshark_file,
+				tap: data['_analyse']
+			}));
 		anal_a.addEventListener("click", window.webshark.popup_on_click_a);
 
 		var glyph = m_webshark_symbols_module.webshark_glyph_img('analyse', 16);
@@ -389,7 +393,11 @@ function webshark_create_tap_action_common(data)
 		var filter_a = document.createElement('a');
 
 		filter_a.setAttribute("target", "_blank");
-		filter_a.setAttribute("href", window.webshark.webshark_get_url() + "&filter=" + encodeURIComponent(data['_filter']));
+		filter_a.setAttribute("href", window.webshark.webshark_create_url(
+			{
+				file: g_webshark_file,
+				filter: data['_filter']
+			}));
 		filter_a.addEventListener("click", window.webshark.popup_on_click_a);
 
 		var glyph = m_webshark_symbols_module.webshark_glyph_img('filter', 16);
@@ -405,7 +413,11 @@ function webshark_create_tap_action_common(data)
 		var show_a = document.createElement('a');
 
 		show_a.setAttribute("target", "_blank");
-		show_a.setAttribute("href", window.webshark.webshark_get_url() + "&frame=" + data['_goto_frame']);
+		show_a.setAttribute("href", window.webshark.webshark_create_url(
+			{
+				file: g_webshark_file,
+				frame: data['_goto_frame']
+			}));
 		show_a.addEventListener("click", window.webshark.popup_on_click_a);
 
 		var glyph = m_webshark_symbols_module.webshark_glyph_img('analyse', 16);
@@ -421,7 +433,12 @@ function webshark_create_tap_action_common(data)
 		var down_a = document.createElement('a');
 
 		down_a.setAttribute("target", "_blank");
-		down_a.setAttribute("href", g_webshark_url + 'req=download&capture=' + g_webshark_file  + "&token=" + encodeURIComponent(data['_download']));
+		down_a.setAttribute("href", window.websharl.webshark_create_api_url(
+			{
+				req: 'download',
+				capture: g_webshark_file,
+				token: data['_download']
+			}));
 		down_a.addEventListener("click", window.webshark.popup_on_click_a);
 
 		var glyph = m_webshark_symbols_module.webshark_glyph_img('download', 16);
@@ -443,7 +460,12 @@ function webshark_create_tap_action_common(data)
 		down_a.setAttribute("target", "_blank");
 		down_a["ws_title"] = descr;
 		down_a["ws_rtp"] = data['_play'];
-		down_a.setAttribute("href", g_webshark_url + 'req=download&capture=' + g_webshark_file  + "&token=" + encodeURIComponent(data['_play']));
+		down_a.setAttribute("href", window.webshark.webshark_create_api_url(
+			{
+				req: 'download',
+				capture: g_webshark_file,
+				token: data['_play']
+			}));
 		down_a.addEventListener("click", m_webshark_rtp_player_module.play_on_click_a);
 
 		var glyph = m_webshark_symbols_module.webshark_glyph_img('play', 16);

web/config.html
@@ -12,7 +12,7 @@
 	<script src="js/webshark-app.js" type="text/javascript"></script>
 
 	<script type="text/javascript">
-		var g_webshark_url = '/webshark/json?';
+		var g_webshark_url = '/webshark/json';
 
 		var g_webshark_preferences = null;
 

web/index.html
@@ -16,7 +16,7 @@
 
 	<script type="text/javascript">
 		var g_webshark_file = "";
-		var g_webshark_url = '/webshark/json?';
+		var g_webshark_url = '/webshark/json';
 		var g_webshark_on_frame_change = null;
 		var g_webshark_prototree_html = null;
 		var g_webshark_hexdump = null;
@@ -129,7 +129,12 @@ TODO: Give user possible to generate multiple reports by selecting checkboxes
 
 				a.appendChild(document.createTextNode(title));
 				a.setAttribute("target", "_blank");
-				a.setAttribute("href", window.location.href + "&tap=" + tap);
+				a.setAttribute("href", window.webshark.webshark_create_url(
+					{
+						file: g_webshark_file,
+						tap: tap
+					}));
+
 				a.setAttribute("id", "menu_tap_" + tap);
 				// a.addEventListener("click", window.webshark.popup_on_click);
 
@@ -243,7 +248,12 @@ TODO: Give user possible to generate multiple reports by selecting checkboxes
 				{
 					var down_a = document.getElementById('toolbar_capture_down');
 					down_a.setAttribute("target", "_blank");
-					down_a.setAttribute("href", g_webshark_url + 'req=download&capture=' + g_webshark_file  + "&token=self");
+					down_a.setAttribute("href", window.webshark.webshark_create_api_url(
+						{
+							req: 'download',
+							capture: g_webshark_file,
+							token: 'self'
+						}));
 					down_a.addEventListener("click", window.webshark.popup_on_click_a);
 
 					var glyph = window.webshark.webshark_glyph_img('download', 16);

web/js/webshark-capture-files.js
@@ -205,7 +205,10 @@ WSCaptureFilesTable.prototype._onRowClickHTML = function(click_tr, file)
 		return;
 	}
 
-	file['url'] = window.webshark.webshark_get_base_url() + '?file=' + file['_path'];
+	file['url'] = window.webshark.webshark_create_url(
+		{
+			file: file['_path']
+		});
 
 	if (this.fileDetails != null)
 	{
@@ -244,7 +247,10 @@ WSCaptureFilesTable.prototype._createFileRowHTML = function(file, row_no)
 	{
 		data[0] = file['_path'];
 
-		a_href.setAttribute("href", window.webshark.webshark_get_base_url() + "?dir=" + file['_path']);
+		a_href.setAttribute("href", window.webshark.webshark_create_url(
+			{
+				dir: file['_path']
+			}));
 		a_href.addEventListener("click",
 			function(ev)
 			{
@@ -256,7 +262,10 @@ WSCaptureFilesTable.prototype._createFileRowHTML = function(file, row_no)
 	}
 	else
 	{
-		a_href.setAttribute("href", window.webshark.webshark_get_base_url() + "?file=" + file['_path']);
+		a_href.setAttribute("href", window.webshark.webshark_create_url(
+			{
+				file: file['_path']
+			}));
 	}
 	a_href.appendChild(document.createTextNode(data[0]));
 

web/js/webshark-packet-list.js
@@ -54,7 +54,11 @@ td.width = Math.floor(1000 / cols.length) + "px"; // XXX, temporary
 				a.appendChild(document.createTextNode(cols[j]))
 
 				a.setAttribute("target", "_blank");
-				a.setAttribute("href", window.webshark.webshark_get_url() + "&frame=" + fnum);
+				a.setAttribute("href", window.webshark.webshark_create_url(
+					{
+						file: g_webshark_file,
+						frame: fnum
+					}));
 				a.addEventListener("click", window.webshark.popup_on_click_a);
 
 				td.appendChild(a);
@@ -69,7 +73,11 @@ td.width = Math.floor(1000 / cols.length) + "px"; // XXX, temporary
 				comment_glyph.setAttribute('title', 'Comment');
 
 				a.setAttribute("target", "_blank");
-				a.setAttribute("href", window.webshark.webshark_get_url() + "&frame=" + fnum);
+				a.setAttribute("href", window.webshark.webshark_create_url(
+					{
+						file: g_webshark_file,
+						frame: fnum
+					}));
 				a.addEventListener("click", window.webshark.webshark_frame_comment_on_click);
 				a.addEventListener("mouseover", window.webshark.webshark_frame_comment_on_over);
 

web/js/webshark-protocol-tree.js
@@ -109,7 +109,11 @@ ProtocolTree.prototype.create_subtree = function(tree, proto_tree, level)
 			a.appendChild(txt_node);
 
 			a.setAttribute("target", "_blank");
-			a.setAttribute("href", window.webshark.webshark_get_url() + "&frame=" + finfo['fnum']);
+			a.setAttribute("href", window.webshark.webshark_create_url(
+				{
+					file: g_webshark_file,
+					frame: finfo['fnum']
+				}));
 			a.addEventListener("click", window.webshark.webshark_frame_goto);
 
 			a.data_ws_frame = finfo['fnum'];
@@ -136,7 +140,11 @@ ProtocolTree.prototype.create_subtree = function(tree, proto_tree, level)
 
 			filter_a.setAttribute("target", "_blank");
 			filter_a.setAttribute("style", "float: right;");
-			filter_a.setAttribute("href", window.webshark.webshark_get_url() + "&filter=" + encodeURIComponent(finfo['f']));
+			filter_a.setAttribute("href", window.webshark.webshark_create_url(
+				{
+					file: g_webshark_file,
+					filter: finfo['f']
+				}));
 			filter_a.addEventListener("click", window.webshark.popup_on_click_a);
 			/* filter_a.addEventListener("click", webshark_tap_row_on_click_filter.bind(null, finfo['f'])); */
 

web/js/webshark.js
@@ -343,6 +343,44 @@ function popup_on_click_a(ev)
 	}
 }
 
+function webshark_params_to_uri(params)
+{
+	var req = null;
+
+	for (var r in params)
+	{
+		var creq = r + "=" + encodeURIComponent(params[r]);
+
+		if (req)
+			req = req + "&" + creq;
+		else
+			req = '?' + creq;
+	}
+
+	if (req == null)
+		req = '';
+
+	return req;
+}
+
+function webshark_create_url(params)
+{
+	var base_url = window.location.href.split("?")[0];
+
+	var str_params = webshark_params_to_uri(params);
+
+	return base_url + str_params;
+}
+
+function webshark_create_api_url(params)
+{
+	var base_url = g_webshark_url;
+
+	var str_params = webshark_params_to_uri(params);
+
+	return base_url + str_params;
+}
+
 function webshark_get_params_url()
 {
 	var query = window.location.href.split("?")[1];
@@ -360,22 +398,6 @@ function webshark_get_params_url()
 	}
 
 	return result;
-}
-
-function webshark_get_base_url()
-{
-	var base_url = window.location.href.split("?")[0];
-
-	return base_url;
-}
-
-function webshark_get_url()
-{
-	var base_url = window.location.href.split("?")[0];
-
-	var extra = '?file=' + g_webshark_file;
-
-	return base_url + extra;
 }
 
 function webshark_d3_chart(svg, data, opts)
@@ -674,21 +696,13 @@ function webshark_json_get(req_data, cb)
 {
 	var http = new XMLHttpRequest();
 
-	var req = null;
+	var url = webshark_create_api_url(req_data);
 
-	for (var r in req_data)
-	{
-		var creq = r + "=" + encodeURIComponent(req_data[r]);
-
-		if (req)
-			req = req + "&" + creq;
-		else
-			req = creq;
-	}
+	var req = JSON.stringify(req_data);
 
 	debug(3, " webshark_json_get(" + req + ") sending request");
 
-	http.open("GET", g_webshark_url + req, true);
+	http.open("GET", url, true);
 	http.onreadystatechange =
 		function()
 		{
@@ -878,7 +892,12 @@ function webshark_load_frame(framenum, scroll_to, cols)
 
 					if (it)
 					{
-						it.setAttribute("href", window.location.href + "&follow=" + fol[i][0] + '&filter=' + fol[i][1]);
+						it.setAttribute("href", webshark_create_url(
+							{
+								file: g_webshark_file,
+								follow: fol[i][0],
+								filter: fol[i][1]
+							}));
 						it.style.display = 'inline';
 					}
 				}
@@ -990,9 +1009,9 @@ exports.webshark_glyph_img = m_webshark_symbols_module.webshark_glyph_img;
 exports.Webshark = Webshark;
 exports.webshark_json_get = webshark_json_get;
 
+exports.webshark_create_url = webshark_create_url;
+exports.webshark_create_api_url = webshark_create_api_url;
 exports.webshark_get_params_url = webshark_get_params_url;
-exports.webshark_get_base_url = webshark_get_base_url;
-exports.webshark_get_url = webshark_get_url;
 exports.webshark_frame_goto = webshark_frame_goto;
 exports.webshark_load_frame = webshark_load_frame;
 exports.popup_on_click_a = popup_on_click_a;

web/upload.html
@@ -12,7 +12,7 @@
 	<script src="js/webshark-app.js" type="text/javascript"></script>
 
 	<script type="text/javascript">
-		var g_webshark_url = '/webshark/json?';
+		var g_webshark_url = '/webshark/json';
 
 		var g_SIZE_LIMIT = 10 * 1024 * 1024; /* 10 MB */
 

commit 8129b7fb8b510e17f6d5760f8a2d14ada39ae25e

webshark: fix typo
regression from 725a7ec2b113543de74ed15e70960ee6fd7b6370

web/js/webshark-tap.js
@@ -434,7 +434,7 @@ function webshark_create_tap_action_common(data)
 		var down_a = document.createElement('a');
 
 		down_a.setAttribute("target", "_blank");
-		down_a.setAttribute("href", window.websharl.webshark_create_api_url(
+		down_a.setAttribute("href", window.webshark.webshark_create_api_url(
 			{
 				req: 'download',
 				capture: g_webshark_file,